2547920e2d8d54adbb822b92d0e90e2aaf999f21
dougatbold <doug@boldxchange.com>
2024-12-07 15:08:15 -0600

    updated methods.

--- basics/basics-05/app.js
+++ basics/basics-05/app.js
@@ -2,12 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
+    },
+    outputFullname() {
+      if (this.name === "") {
+        return "";
+      }
+
+      return this.name + " Spencer";
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +22,13 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+    resetInput() {
+      this.name = ``;
+    },
+  },
 });
 
-app.mount('#events');
+console.log("Initializing Vue app...");
+const vm = app.mount("#events");
+console.log("Vue app mounted, current name:", vm.name);

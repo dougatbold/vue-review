f6a45b18398cf59e094dfd690aedefbd4e28468c
dougatbold <doug@boldxchange.com>
2024-12-07 13:50:35 -0600

    finished basics-03 added basics-05.

--- basics/basics-03/app.js
+++ basics/basics-03/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ``,
+      confirmedName: ``,
     };
+  },
+  methods: {
+    counterAdd(num) {
+      this.counter = this.counter + num;
+    },
+    counterSubtract(num) {
+      this.counter = this.counter - num;
+    },
+    setName(e, lastName) {
+      this.name = e.target.value + ` ` + lastName;
+    },
+    submitForm() {
+      alert(`Submitted!`);
+    },
+    submittedName() {
+      this.confirmedName = this.name;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");

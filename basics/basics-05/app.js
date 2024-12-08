a489be43af9671f82ca743d16dc0f2adf24feb04
dougatbold <doug@boldxchange.com>
2024-12-08 06:34:52 -0600

    reactivity and assignment 03.

--- basics/basics-05/app.js
+++ basics/basics-05/app.js
@@ -3,7 +3,41 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      // fullname: "",
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     return "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     return "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
+  },
+  computed: {
+    fullname() {
+      if (this.name === "" || this.lastName) {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
     setName(event) {

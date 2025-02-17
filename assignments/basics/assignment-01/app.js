6da71915f2f6b191302d1d1cf02fa8d64eea6cc3
dougatbold <doug@boldxchange.com>
2025-02-17 07:46:31 -0600

    Recreated assignment 1.

--- assignments/basics/assignment-01/app.js
+++ assignments/basics/assignment-01/app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: `Doug`,
+      name: `Doug Spencer`,
       age: 31,
-      photo: `https://thegrindfs.com/cdn/shop/files/The_Grind_Logo_Transparent_480x.png?v=1627693918`,
+      randomPhoto: `https://images.sidearmdev.com/resize?url=https%3A%2F%2Fdxbhsrqyrr690.cloudfront.net%2Fsidearm.nextgen.sites%2Fsoonersports.com%2Fimages%2F2013%2F8%2F7%2FQTQHDXOVUSUMUNZ.20130807133230.jpg&height=300&type=webp`,
     };
   },
   methods: {
-    ageInFive() {
-      return this.age + 5;
+    addYears(years) {
+      const newAge = this.age + years;
+      return newAge;
     },
     randomNumber() {
       return Math.random();

69166c1f83460aa2e3b9eaf2437bebad1e61ae6a
dougatbold <doug@boldxchange.com>
2025-02-21 08:06:07 -0600

    conditional logic basics.

--- conditional-and-lists/app.js
+++ conditional-and-lists/app.js
@@ -1,18 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      enteredGoalValue: "",
-      goals: [],
+      enteredGoalValue: ``,
+      goals: [`Testing`],
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
     },
-    removeGoal(idx) {
-      this.goals.splice(idx, 1);
-    },
   },
 });
 
-app.mount("#user-goals");
+app.mount(`#user-goals`);
